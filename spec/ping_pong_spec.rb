32fdecec5ab8050c6483cd63d9303057e224db73
Steve Schneider <steve.schneider99@gmail.com>
2015-05-18 11:28:56 -0700

    Refactor
    Remove extra lines for spacing.

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -17,5 +17,4 @@ describe('ping_pong') do
   it("returns an array with ping-pong if number is divisible by 3 and 5") do
     expect((15).ping_pong).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong"]))
   end
-
 end
